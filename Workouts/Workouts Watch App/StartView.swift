3a4e6d7a0fa25d08d7d57cf0c03aa41da94aa6fc
hyun99999 <ypsophila97@icloud.com>
2022-11-03 14:34:05 +0900

    Chore: add project file

---
+++ Workouts/Workouts Watch App/StartView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Workouts Watch App
+//
+//  Created by kimhyungyu on 2022/11/03.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartView()
+    }
+}
---
+++ Workouts/Workouts Watch App/WorkoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutsApp.swift
+//  Workouts Watch App
+//
+//  Created by kimhyungyu on 2022/11/03.
+//
+
+import SwiftUI
+
+@main
+struct Workouts_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            StartView()
+        }
+    }
+}
