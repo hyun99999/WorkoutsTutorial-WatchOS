114442799fe9638b6a0d9ff1e48a0a1a121aaf2e
hyun99999 <ypsophila97@icloud.com>
2022-11-05 00:39:41 +0900

    Add: implement MetricsView
    - that display a metrics about state.

---
+++ Workouts/Workouts Watch App/MetricsView.swift
@@ -0,0 +1,59 @@
+//
+//  MetricsView.swift
+//  Workouts Watch App
+//
+//  Created by kimhyungyu on 2022/11/04.
+//
+
+import SwiftUI
+
+struct MetricsView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("03:15.23")
+                .foregroundColor(Color.yellow)
+                .fontWeight(.semibold)
+            Text(
+                Measurement(
+                    value: 47,
+                    unit: UnitEnergy.kilocalories
+                ).formatted(
+                    .measurement(
+                        width: .abbreviated,
+                        usage: .workout,
+                        numberFormatStyle: .number)
+                )
+            )
+            Text(
+                153.formatted(
+                    .number.precision(.fractionLength(0))
+                )
+                + " bpm"
+            )
+            Text(
+                Measurement(
+                    value: 515,
+                    unit: UnitLength.meters
+                ).formatted(
+                    .measurement(
+                        width: .abbreviated,
+                        usage: .road
+                    )
+                )
+            )
+        }
+        .font(.system(.title, design: .rounded)
+                .monospacedDigit()
+                .lowercaseSmallCaps()
+        )
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .ignoresSafeArea(edges: .bottom)
+        .scenePadding()
+    }
+}
+
+struct MetricsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MetricsView()
+    }
+}
