c850eb6ac47c9733f47f9ed91d070e786010b41d
hyun99999 <ypsophila97@icloud.com>
2022-11-05 01:33:10 +0900

    Add: implement ControlsView
    - that display end and pause button.

---
+++ Workouts/Workouts Watch App/ControlsView.swift
@@ -0,0 +1,39 @@
+//
+//  ControlsView.swift
+//  Workouts Watch App
+//
+//  Created by kimhyungyu on 2022/11/05.
+//
+
+import SwiftUI
+
+struct ControlsView: View {
+    var body: some View {
+        HStack {
+            VStack {
+                Button {
+                } label: {
+                    Image(systemName: "xmark")
+                }
+                .tint(Color.red)
+                .font(.title2)
+                Text("End")
+            }
+            VStack {
+                Button {
+                } label: {
+                    Image(systemName: "pause")
+                }
+                .tint(Color.yellow)
+                .font(.title2)
+                Text("Pause")
+            }
+        }
+    }
+}
+
+struct ControlsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlsView()
+    }
+}
