cd7f4505191b8cd710e24098f281b56cd98f5631
hyun99999 <ypsophila97@icloud.com>
2022-11-08 12:16:08 +0900

    Add: implement SummaryView
    - display common view structure that has title, value.

---
+++ Workouts/Workouts Watch App/SummaryView.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct SummaryView: View {
+    @State private var durationFormatter: DateComponentsFormatter = {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute, .second]
+        formatter.zeroFormattingBehavior = .pad
+        return formatter
+    }()
+    
+    var body: some View {
+    }
+}
+
+struct SummaryMetricView: View {
+    var title: String
+    var value: String
+
+    var body: some View {
+        Text(title)
+        Text(value)
+            .font(.system(.title2, design: .rounded)
+                    .lowercaseSmallCaps()
+            )
+            .foregroundColor(.accentColor)
+        Divider()
+    }
+}
+
+struct SummaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        SummaryView()
+    }
+}
