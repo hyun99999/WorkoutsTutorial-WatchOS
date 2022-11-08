604769b46723c7283c9424ffcde292a1b9709e74
hyun99999 <ypsophila97@icloud.com>
2022-11-08 12:21:00 +0900

    feat: implement SummaryView with SummaryMetricView

--- Workouts/Workouts Watch App/SummaryView.swift
+++ Workouts/Workouts Watch App/SummaryView.swift
@@ -1,3 +1,10 @@
+//
+//  SummaryView.swift
+//  Workouts Watch App
+//
+//  Created by kimhyungyu on 2022/11/08.
+//
+
 import SwiftUI
 
 struct SummaryView: View {
@@ -9,6 +16,51 @@ struct SummaryView: View {
     }()
     
     var body: some View {
+        ScrollView(.vertical) {
+            VStack(alignment: .leading) {
+                SummaryMetricView(
+                    title: "Total Time",
+                    value: durationFormatter.string(from: 30 * 60 + 15) ?? ""
+                ).accentColor(Color.yellow)
+                SummaryMetricView(
+                    title: "Total Distance",
+                    value: Measurement(
+                        value: 1625,
+                        unit: UnitLength.meters
+                    ).formatted(
+                        .measurement(
+                            width: .abbreviated,
+                            usage: .road
+                        )
+                    )
+                ).accentColor(Color.green)
+                SummaryMetricView(
+                    title: "Total Energy",
+                    value: Measurement(
+                        value: 96,
+                        unit: UnitEnergy.kilocalories
+                    ).formatted(
+                        .measurement(
+                            width: .abbreviated,
+                            usage: .workout,
+                            numberFormatStyle: .number)
+                    )
+                ).accentColor(Color.pink)
+                SummaryMetricView(
+                    title: "Avg. Heart Rate",
+                    value: 143
+                        .formatted(
+                            .number.precision(.fractionLength(0))
+                        )
+                    + " bpm"
+                ).accentColor(Color.red)
+                Button("Done") {
+                }
+            }
+            .scenePadding()
+        }
+        .navigationTitle("Summary")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
