08616eb1c38357c966bbcff1406eee1d2868d4ef
hyun99999 <ypsophila97@icloud.com>
2022-11-11 17:40:55 +0900

    feat: set up HealthKit and request authorization
    - to read and share health data.

--- Workouts/Workouts Watch App/WorkoutManager.swift
+++ Workouts/Workouts Watch App/WorkoutManager.swift
@@ -50,4 +50,26 @@ class WorkoutManager: NSObject, ObservableObject {
             // The workout has started.
         }
     }
+    
+    /// Request authorization to access HealthKit.
+    func requestAuthorization() {
+        // The quantity type to write to the health store.
+        let typesToShare: Set = [
+            HKQuantityType.workoutType()
+        ]
+
+        // The quantity types to read from the health store.
+        let typesToRead: Set = [
+            HKQuantityType.quantityType(forIdentifier: .heartRate)!,
+            HKQuantityType.quantityType(forIdentifier: .activeEnergyBurned)!,
+            HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning)!,
+            HKQuantityType.quantityType(forIdentifier: .distanceCycling)!,
+            HKObjectType.activitySummaryType()
+        ]
+
+        // Request authorization for those quantity types.
+        healthStore.requestAuthorization(toShare: typesToShare, read: typesToRead) { (success, error) in
+            // Handle error.
+        }
+    }
 }
