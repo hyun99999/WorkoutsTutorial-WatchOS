3b4284c63598f21aee27c50beb59a3302aef413f
hyun99999 <ypsophila97@icloud.com>
2022-11-14 20:12:23 +0900

    add: feat TimelineView to MetricsView
    - ElapsedView isn't published, so update time with TimelineView and MetricsTimelineSchedule.

--- Workouts/Workouts Watch App/MetricsView.swift
+++ Workouts/Workouts Watch App/MetricsView.swift
@@ -11,48 +11,55 @@ struct MetricsView: View {
     @EnvironmentObject var workoutManager: WorkoutManager
     
     var body: some View {
-        VStack(alignment: .leading) {
-            ElapsedTimeView(
-                elapsedTime: workoutManager.builder?.elapsedTime ?? 0,
-                showSubseconds: true
-            ).foregroundColor(.yellow)
-            Text(
-                Measurement(
-                    value: workoutManager.activeEnergy,
-                    unit: UnitEnergy.kilocalories
-                ).formatted(
-                    .measurement(
-                        width: .abbreviated,
-                        usage: .workout,
-                        numberFormatStyle: .number)
-                )
+        TimelineView(
+            MetricsTimelineSchedule(
+                from: workoutManager.builder?.startDate ?? Date()
             )
-            Text(
-                workoutManager.heartRate
-                    .formatted(
-                        .number.precision(.fractionLength(0))
-                    )
-                + " bpm"
-            )
-            Text(
-                Measurement(
-                    value: workoutManager.distance,
-                    unit: UnitLength.meters
-                ).formatted(
-                    .measurement(
-                        width: .abbreviated,
-                        usage: .road
+        ) { context in
+            VStack(alignment: .leading) {
+                ElapsedTimeView(
+                    elapsedTime: workoutManager.builder?.elapsedTime ?? 0,
+                    // cadence is live, subseconds are shown. otherwise subseconds are hidden in Always On state.
+                    showSubseconds: context.cadence == .live
+                ).foregroundColor(Color.yellow)
+                Text(
+                    Measurement(
+                        value: workoutManager.activeEnergy,
+                        unit: UnitEnergy.kilocalories
+                    ).formatted(
+                        .measurement(
+                            width: .abbreviated,
+                            usage: .workout,
+                            numberFormatStyle: .number)
                     )
                 )
+                Text(
+                    workoutManager.heartRate
+                        .formatted(
+                            .number.precision(.fractionLength(0))
+                        )
+                    + " bpm"
+                )
+                Text(
+                    Measurement(
+                        value: workoutManager.distance,
+                        unit: UnitLength.meters
+                    ).formatted(
+                        .measurement(
+                            width: .abbreviated,
+                            usage: .road
+                        )
+                    )
+                )
+            }
+            .font(.system(.title, design: .rounded)
+                .monospacedDigit()
+                .lowercaseSmallCaps()
             )
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .ignoresSafeArea(edges: .bottom)
+            .scenePadding()
         }
-        .font(.system(.title, design: .rounded)
-            .monospacedDigit()
-            .lowercaseSmallCaps()
-        )
-        .frame(maxWidth: .infinity, alignment: .leading)
-        .ignoresSafeArea(edges: .bottom)
-        .scenePadding()
     }
 }
 
