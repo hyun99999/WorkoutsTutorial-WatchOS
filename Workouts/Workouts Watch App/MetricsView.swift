158833ba3cbd7155abcf6a860a138c70b88d041a
hyun99999 <ypsophila97@icloud.com>
2022-11-14 20:03:58 +0900

    add: create TimelineSchedule
    - ElapsedView isn't published, so view will not update when builder's elapsedTime updates.
    - this is why we implement TimelineSchedule.

--- Workouts/Workouts Watch App/MetricsView.swift
+++ Workouts/Workouts Watch App/MetricsView.swift
@@ -62,3 +62,23 @@ struct MetricsView_Previews: PreviewProvider {
             .environmentObject(WorkoutManager())
     }
 }
+
+private struct MetricsTimelineSchedule: TimelineSchedule {
+    var startDate: Date
+
+    init(from startDate: Date) {
+        self.startDate = startDate
+    }
+
+    /// produce PeriodicTimelineSchedule with startDate.
+    // interval is determined by the TimelineScheduleMode.
+    func entries(from startDate: Date, mode: TimelineScheduleMode) -> PeriodicTimelineSchedule.Entries {
+        PeriodicTimelineSchedule(
+            from: self.startDate,
+            by: (mode == .lowFrequency ? 1.0 : 1.0 / 30.0)
+        ).entries(
+            from: startDate,
+            mode: mode
+        )
+    }
+}
