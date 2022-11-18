5406de06e57ee0693856e94af0f2059f0a4a3485
hyun99999 <ypsophila97@icloud.com>
2022-11-18 18:55:29 +0900

    fix: update NavigationLink for workoutManager start

--- Workouts/Workouts Watch App/StartView.swift
+++ Workouts/Workouts Watch App/StartView.swift
@@ -17,7 +17,9 @@ struct StartView: View {
         List(workoutTypes) { workoutType in
             NavigationLink(
                 workoutType.name,
-                destination: SessionPagingView()
+                destination: SessionPagingView(),
+                tag: workoutType,
+                selection: $workoutManager.selectedWorkout
             ).padding(EdgeInsets(top: 15, leading: 5, bottom: 15, trailing: 5))
         }
         .listStyle(.carousel)
